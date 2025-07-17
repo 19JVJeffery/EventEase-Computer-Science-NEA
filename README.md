b86fb5f2044a2972dc61d4a6df7ad6ce7465c6ae
James Jeffery <94115228+19JVJeffery@users.noreply.github.com>
2025-07-17 08:19:26 +0000

    Removed files

--- README.md
+++
@@ -1,1 +0,0 @@
-# EventEase-Computer-Science-NEA
--- js/inject.js
+++
@@ -1,9 +0,0 @@
-async function loadFragment(id, url) {
-  const res = await fetch(url);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
-document.addEventListener("DOMContentLoaded", () => {
-  loadFragment("nav-placeholder", "nav.html");
-  loadFragment("footer-placeholder", "footer.html");
-});
