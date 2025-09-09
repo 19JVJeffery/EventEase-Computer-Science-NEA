6258a139658961ddce1bea8760d207bcd6d90e4c
James Jeffery <94115228+19JVJeffery@users.noreply.github.com>
2025-09-09 11:50:53 +0000

    Introduced basic page structure: - index.php - home page - styles.css - style sheet - footer.php - repeated footer component - header.php - repeated header component - events.php - events view page

---
+++ Components/header.php
@@ -0,0 +1,17 @@
+<div class="menuWrapper">
+    <div class="menu">
+      <div class="menuImage">
+        <a href="index.php">
+            <!--Insert logo-->
+            <!--<img src="/Images/Logo.png" alt="Logo">-->
+        </a>
+      </div>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="/events.php">Events</a></li>
+        <li><a href="/contact.php">Contact</a></li>
+        <li><a href="/login.php">Login</a></li>
+        <li><a href="/signup.php">Sign Up</a></li>
+      </ul>
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!--Start of my HTML-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="styles.css">
+    <title>EventEase - Easily book your live events</title>
+  </head>
+  <body>
+  </body>
+</html>
