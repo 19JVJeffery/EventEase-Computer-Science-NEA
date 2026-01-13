f47a36e892b057f9c86c0668a01f982d0e73cb4a
James Jeffery <94115228+19JVJeffery@users.noreply.github.com>
2026-01-13 15:16:01 +0000

    Enhance homepage layout and styling with background image and button improvements

--- index.php
+++ index.php
@@ -16,14 +16,18 @@
     <link rel="stylesheet" href="styles.css">
     <title>EventEase - Easily book your live events</title>
   </head>
-<body class="homePage">
+<body style="background-image: url('pexels-picjumbo-com-55570-196652.jpg'); background-size: cover;">
   <?php include("Components/header.php") ?>
   <div class="bodyBackground">
     <div class="cardsContainer">
-
       <!-- Iterative PHP-->
       <div class="homeCard">
         <h2>Welcome to EventEase!</h2>
+      </div>
+    </div>
+    <br>
+    <div style="margin: 0 auto;">
+          <div class="animated-button" style="margin: 0 auto;">View Events
     </div>
   </div>
 </div>
