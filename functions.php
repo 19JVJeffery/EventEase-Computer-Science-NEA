1171d0fe1f74cdc4a4761c07b7caaa5f74c55390
James Jeffery <94115228+19JVJeffery@users.noreply.github.com>
2026-01-27 14:57:36 +0000

    Add database connection and query function

---
+++ functions.php
@@ -0,0 +1,19 @@
+/**
+ * Connect to the database
+*/
+
+function queryDB($query){
+    //Connect to sqlite file
+    $db = new PDO('sqlite:Tools/EventEase.db');
+
+    //Prepare my query - stops SQL injection attempts
+    $query = $db->prepare($query);
+
+    //Run the queryDB
+    $query->execute();
+
+    //Get the data from the query
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
